8fc6ae5cc5515ba6deb8f15f533ffa5445c06b76
Никита <ikarus11122@gmail.com>
2023-03-07 07:51:44 +0300

    Добавил таблицы в БД

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,14 +1,86 @@
-create table if not exists products (id bigserial primary key, title varchar(255), cost int);
-insert into products (title, cost) values ('milk', 100), ('bread', 200), ('cola', 250), ('pivko',150),('pepsi',230),('apple',78),('banan',160),('kit-kat',50),('orange',135);
-create table users (id bigserial primary key,username   varchar(36) not null,password   varchar(80) not null,email varchar(50) unique,created_at timestamp default current_timestamp,updated_at timestamp default current_timestamp);
-create table roles (id bigserial primary key,name varchar(50) not null,created_at timestamp default current_timestamp,updated_at timestamp default current_timestamp);
+create table if not exists products
+(
+    id
+    bigserial
+    primary
+    key,
+    title
+    varchar
+(
+    255
+),
+    cost int
+    );
 
-CREATE TABLE users_roles (user_id bigint not null references users (id),role_id bigint not null references roles (id),primary key (user_id, role_id));
+insert into products (title, cost)
+values ('milk', 100),
+       ('bread', 200),
+       ('cola', 250),
+       ('pivko', 150),
+       ('pepsi', 230),
+       ('apple', 78),
+       ('banan', 160),
+       ('kit-kat', 50),
+       ('orange', 135);
 
-insert into roles (name) values ('ROLE_USER'),('ROLE_ADMIN');
+create table users
+(
+    id         bigserial primary key,
+    username   varchar(36) not null,
+    password   varchar(80) not null,
+    email      varchar(50) unique,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp
+);
 
-insert into users (username, password, email) values ('bob', '$2a$04$Fx/SX9.BAvtPlMyIIqqFx.hLY2Xp8nnhpzvEEVINvVpwIPbA3v/.i', 'bob_johnson@gmail.com'),('john', '$2a$04$Fx/SX9.BAvtPlMyIIqqFx.hLY2Xp8nnhpzvEEVINvVpwIPbA3v/.i', 'john_johnson@gmail.com');
+create table roles
+(
+    id         bigserial primary key,
+    name       varchar(50) not null,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp
+);
 
-insert into users_roles (user_id, role_id) values (1, 1), (2, 2);
+CREATE TABLE users_roles
+(
+    user_id bigint not null references users (id),
+    role_id bigint not null references roles (id),
+    primary key (user_id, role_id)
+);
+
+insert into roles (name)
+values ('ROLE_USER'),
+       ('ROLE_ADMIN');
+
+insert into users (username, password, email)
+values ('bob', '$2a$04$Fx/SX9.BAvtPlMyIIqqFx.hLY2Xp8nnhpzvEEVINvVpwIPbA3v/.i', 'bob_johnson@gmail.com'),
+       ('john', '$2a$04$Fx/SX9.BAvtPlMyIIqqFx.hLY2Xp8nnhpzvEEVINvVpwIPbA3v/.i', 'john_johnson@gmail.com');
+
+insert into users_roles (user_id, role_id)
+values (1, 1),
+       (2, 2);
+
+create table orders
+(
+    id          bigserial primary key,
+    user_id     bigint not null references users (id),
+    total_price int    not null,
+    address     varchar(255),
+    phone       varchar(255),
+    created_at  timestamp default current_timestamp,
+    updated_at  timestamp default current_timestamp
+);
+
+create table order_items
+(
+    id                bigserial primary key,
+    product_id        bigint not null references products (id),
+    order_id          bigint not null references orders (id),
+    quantity          int    not null,
+    price_per_product int    not null,
+    price             int    not null,
+    created_at        timestamp default current_timestamp,
+    updated_at        timestamp default current_timestamp
+)
 
 
